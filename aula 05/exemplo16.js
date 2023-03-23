1254ce378e9425cbd1d5174561bfc2fcaa9a96ef
Pablo Marcusso <pablomarcussofotos@gmail.com>
2023-03-23 09:09:14 -0300

    Criaçãop do emplo 16

---
+++ aula 05/exemplo16.js
@@ -0,0 +1,21 @@
+class Livro{
+    constructor(pNome, pPreco){
+        this.nome = pNome
+        this.preco = pPreco
+    }
+
+    get Nome(){return this.nome}
+    set Nome(pNome) {this.nome = pNome}
+
+    get Preco(){return this.preco}
+    set Preco(pPreco) {this.nome = pPreco}
+
+    calcularDesconto(){
+        this.preco = this.preco - 10
+    }
+
+}
+
+var obj_Livro1 = new Livro ("Use a cabeça Java",200)
+obj_Livro1.calcularDesconto()
+console.log("Nome do Livro: " + obj_Livro1.nome + "custa :" + obj_Livro1.preco + "reais")

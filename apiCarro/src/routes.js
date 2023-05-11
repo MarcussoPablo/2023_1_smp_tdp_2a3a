cfa5dc187f317e953d49439afaf392e385cfced7
Pablo Marcusso <pablomarcussofotos@gmail.com>
2023-05-11 10:40:29 -0300

    feat(apiCarro)segunda aula de api

--- apiCarro/src/routes.js
+++ apiCarro/src/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const CarroController = require('./controllers/CarroController') 
+const CarroController = require('./controllers/CarroController');
+
+router.get('/carros',CarroController.buscarTodos)
 
 module.exports = router;

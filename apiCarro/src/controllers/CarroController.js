babda2525338b2598d74e40131cef6a80a9151ac
Pablo Marcusso <pablomarcussofotos@gmail.com>
2023-05-18 10:33:34 -0300

    feat(apiCarro) finalizando o deletar

--- apiCarro/src/controllers/CarroController.js
+++ apiCarro/src/controllers/CarroController.js
@@ -4,6 +4,7 @@ module.exports = {
     buscarTodos: async(req, res) =>{
         let json = {error:'', result:[]};
 
+        
         let carros = await CarroService.buscarTodos();
 
         for (let i in carros){
@@ -14,5 +15,51 @@ module.exports = {
             });
         }
         res.json(json);
-    }
+    },
+
+    buscarUm: async(req, res) =>{
+        let json = {error:'', result:[]};
+
+        
+        let codigo = req.params.codigo;
+        let carro = await CarroService.buscarUm(codigo);
+
+        if(carro){
+            json.result = carro;
+        }
+        res.json(json);
+    },
+
+    inserir: async(req, res) =>{
+        let json = {error:'', result:[]};
+
+        
+        let modelo = req.body.modelo;
+        let placa = req.body.placa;
+     
+
+        if(modelo && placa){
+            let carroCodigo = await CarroService.inserir(modelo, placa);
+            json.result = {
+                codigo: carroCodigo,modelo,placa
+            };
+        }else{
+            json.error = 'Campos não enviados'
+        }
+        res.json(json);
+    },
+      
+    deletar: async (req, res) => {
+        let json = { error: '', result: [] };
+      
+        try {
+          const codigo = req.params.codigo;
+          await CarroService.deletar(codigo);
+          json.result = 'Carro deletado com sucesso';
+        } catch (error) {
+          json.error = 'Ocorreu um erro ao deletar o carro';
+        }
+      
+        res.json(json);
+      }
 }
--- apiCarro/src/routes.js
+++ apiCarro/src/routes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const CarroController = require('./controllers/CarroController');
 
-router.get('/carros',CarroController.buscarTodos)
+router.get('/carros',CarroController.buscarTodos);
+router.get('/carro/:codigo',CarroController.buscarUm);
+router.post('/carro',CarroController.inserir);
+router.delete('/carro/:codigo', CarroController.deletar);
 
 module.exports = router;
